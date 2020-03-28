929eb6884baef928e22d95072d2ccbe3de5a0151
pop_club <3234974028@qq.com>
2020-03-28 17:04:31 +0800

    表格分页

--- src/views/Account.js
+++ src/views/Account.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import styles from './Account.module.scss'
 import Input from '@/components/Input/Input'
-import { Button, Table } from 'antd'
+import { Button } from 'antd'
+import Info from './Info'
 
 export default class Account extends Component {
   constructor (props) {
@@ -30,47 +31,16 @@ export default class Account extends Component {
   }
 
   render() {
-    const dataSource = [
-      {
-        key: '1',
-        name: '胡彦斌',
-        age: 32,
-        address: '西湖区湖底公园1号',
-      },
-      {
-        key: '2',
-        name: '胡彦祖',
-        age: 42,
-        address: '西湖区湖底公园1号',
-      },
-    ]
-    const columns = [
-      {
-        title: '姓名',
-        dataIndex: 'name',
-        key: 'name',
-      },
-      {
-        title: '年龄',
-        dataIndex: 'age',
-        key: 'age',
-      },
-      {
-        title: '住址',
-        dataIndex: 'address',
-        key: 'address',
-      },
-    ]
     return (
       <div className={styles.account}>
         <div className={styles.search}>
           <div className={styles.row}>
             <label>账号：</label>
-            <Input emit={this.setFrom} index="account"/>
+            <Input emit={this.setFrom} index="account"></Input>
           </div>
           <div className={styles.row}>
             <label>真实姓名：</label>
-            <Input emit={this.setFrom} index="realname"/>
+            <Input emit={this.setFrom} index="realname"></Input>
           </div>
           <div className={styles.row}>
             <Button onClick={this.log} type="primary" className={styles['ant-btn']}>查询</Button>
@@ -79,9 +49,8 @@ export default class Account extends Component {
         </div>
         <div className={styles.content}>
           <div className={styles.table}>
-            <Table dataSource={dataSource} columns={columns} />
+            <Info></Info>
           </div>
-          <div className={styles.page}></div>
         </div>
       </div>
     )
---
+++ src/views/Info.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import { Table } from 'antd'
+
+export default class Info extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loading: true,
+      dataSource: null,
+      pagination: {
+        current: 1,
+        pageSize: 10,
+        total: 55,
+        onChange: this.onChange,
+        showTotal: total => `共${total}条`,
+      }
+    }
+  }
+
+  componentDidMount () {
+    this.fetch()
+  }
+
+  onChange = (current, pageSize) => {
+    const pagination = Object.assign({}, this.state.pagination, {
+      current,
+      pageSize
+    })
+    this.setState({ pagination })
+    this.fetch()
+  }
+
+  fetch = () => {
+    this.setState({
+      loading: true
+    })
+    setTimeout(() => {
+      const dataSource = []
+      for (let i=0; i<10; i++) {
+        dataSource.push({
+          key: i,
+          name: '胡彦斌',
+          age: i * i,
+          address: `西湖区湖底公园${i}号`,
+        })
+      }
+      this.setState({
+        loading: false,
+        dataSource
+      })
+    }, 1000)
+  }
+
+  render() {
+    const columns = [
+      {
+        title: '序号',
+        dataIndex: 'key',
+        key: 'key',
+        render: (text,record,index) => `${index + 1}`
+      },
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+      },
+      {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+      },
+    ]
+    return (
+      <div>
+        <Table loading={this.state.loading} dataSource={this.state.dataSource} columns={columns} pagination={this.state.pagination}></Table>
+      </div>
+    )
+  }
+}
